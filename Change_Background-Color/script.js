ec9f53915556c1af08ab3bcf1324e6c0eb69cd5f
Shaguftarubab <165014843+Shaguftarubab@users.noreply.github.com>
2024-08-20 23:31:48 +0500

    Update script.js

---
+++ Change_Background-Color/script.js
@@ -0,0 +1,27 @@
+*{
+    margin: 0;
+    padding: 0;
+}
+.canvas{
+    height: 100vh;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.671);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    row-gap: 30px;
+
+}
+.msg{
+    color: white;
+    font-size: 3rem;
+
+}
+button{
+    background-color: #f0f2fc;
+    border: 5px solid ;
+    border-radius: 8%;
+    font-size: 2rem;
+
+}
