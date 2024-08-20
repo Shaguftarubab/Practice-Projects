a840cbfda05faa7af09e452d97ba5b1304635ee8
Shaguftarubab <165014843+Shaguftarubab@users.noreply.github.com>
2024-08-20 23:33:26 +0500

    Update script.js

--- Change_Background-Color/script.js
+++ Change_Background-Color/script.js
@@ -1,1 +1,7 @@
-
+const colors=["red","blue","purple","wheat","yellow","black","grey","green","pink","aqua"];
+const btn=document.querySelector("button");
+btn=addEventListener("click",()=>{
+const ind=Math.floor(Math.random()*colors.length+1);
+let canvas=document.querySelector("body");
+canvas.style.backgroundColor=colors[ind];
+});
